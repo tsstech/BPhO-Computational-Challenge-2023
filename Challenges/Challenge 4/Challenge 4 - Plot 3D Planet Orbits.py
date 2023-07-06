dbaec0b0de2b31bc3eb46b3da00e9bea1c568f61
V Shah <vidisha156@yahoo.com>
2023-07-06 16:18:39 +0100

    Completed a class for vectors

---
+++ Challenges/Challenge 4/Challenge 4 - Plot 3D Planet Orbits.py
@@ -0,0 +1,81 @@
+import math
+
+########## CLASS FOR VECTORS ####### 
+class Vector:
+    ## initialiser
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y}, {self.z})"
+
+    def __str__(self):
+        return f"{self.x}i + {self.y}j + {self.z}k"
+
+    ## indexing vector
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        elif item == 1:
+            return self.y
+        elif item == 2:
+            return self.z
+        else:
+            raise IndexError("There are only three elements in the vector")
+
+    ## adding to the vector
+    def __add__(self, other):
+        return Vector(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+        )
+
+    ## subtracting from the vector
+    def __sub__(self, other):
+        return Vector(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+        )
+
+    ## multiplying with the vector
+    def __mul__(self, other):
+        if isinstance(other, Vector):  # Vector dot product
+            return (
+                self.x * other.x
+                + self.y * other.y
+                + self.z * other.z
+            )
+        elif isinstance(other, (int, float)):  # Scalar multiplication
+            return Vector(
+                self.x * other,
+                self.y * other,
+                self.z * other,
+            )
+        else:
+            raise TypeError("operand must be Vector, int, or float")
+
+    ## dividing with vector
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(
+                self.x / other,
+                self.y / other,
+                self.z / other,
+            )
+        else:
+            raise TypeError("operand must be int or float")
+        
+    def get_magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def normalize(self):
+        magnitude = self.get_magnitude()
+        return Vector(
+            self.x / magnitude,
+            self.y / magnitude,
+            self.z / magnitude,
+        )
